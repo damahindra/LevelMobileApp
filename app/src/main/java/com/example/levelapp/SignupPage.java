f82c292f88080687a2252ce758a1858d6e81a3e7
ranggaandhito <91388757+ranggaandhito@users.noreply.github.com>
2023-05-23 22:34:13 +0700

    Finished SignUp Page

--- app/src/main/java/com/example/levelapp/SignupPage.java
+++ app/src/main/java/com/example/levelapp/SignupPage.java
@@ -3,12 +3,22 @@ package com.example.levelapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SignupPage extends AppCompatActivity {
+    EditText user_email, user_pw;
+    Button signup;
+    TextView toSignin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_page);
+        user_email = findViewById(R.id.user_email);
+        user_pw = findViewById(R.id.user_password);
+        signup = findViewById(R.id.signup_btn);
+        toSignin = findViewById(R.id.toSignin);
     }
 }
