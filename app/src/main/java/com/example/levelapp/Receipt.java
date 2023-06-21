aa3f09d36469b4391f6c31d9b6fbdabb7df72b1d
dennisricco <99968743+dennisricco@users.noreply.github.com>
2023-06-21 14:19:44 +0700

    Initial Commit

--- app/src/main/java/com/example/levelapp/Receipt.java
+++ app/src/main/java/com/example/levelapp/Receipt.java
@@ -3,13 +3,19 @@ package com.example.levelapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Receipt extends AppCompatActivity {
+
+    TextView Noresi, nama, email, destinasi, lokasidestinasi, baiaya;
+
+    Button Download;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receipt);
-//        bagian dennis goes here...
+//
     }
 }
