e88da16496f4335d12ec9964f72afeaeccf7c176
damahindra <ranggaandhito@gmail.com>
2023-06-22 13:58:53 +0700

    added history

--- app/src/main/java/com/example/levelapp/History.java
+++ app/src/main/java/com/example/levelapp/History.java
@@ -1,14 +1,139 @@
 package com.example.levelapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.levelapp.MainPage.Wisata;
+import com.example.levelapp.Model.UserData;
+import com.example.levelapp.Transaction.Transaction;
+import com.example.levelapp.Transaction.TransactionAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class History extends AppCompatActivity {
+    RecyclerView rv_history;
+    List<Transaction> transactionList;
+    Button clear;
+
+//    fetch transaction data from Firebase
+    FirebaseDatabase userDatabase;
+    DatabaseReference databaseRef;
+
+    FirebaseAuth mAuth;
+    FirebaseUser currentUser;
+    String userPath;
+
+    String fullName;
+
+    String fetched_name, fetched_location_name, fetched_location, fetched_dateTime, fetched_email;
+    int fetched_qty;
+    long fetched_price;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+        rv_history = findViewById(R.id.rv_history);
+        transactionList = new ArrayList<>();
+
+        clear = findViewById(R.id.btn_delete_history);
+
+        userDatabase = FirebaseDatabase.getInstance();
+        databaseRef = userDatabase.getReference();
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+
+        if (currentUser != null) userPath = "users/" + currentUser.getEmail().replaceAll("@gmail.com", "");
+
+        databaseRef.child(userPath).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                UserData userData = snapshot.getValue(UserData.class);
+                if (userData != null) {
+                    fullName = userData.getNamaDepan() + " " + userData.getNamaBelakang();
+
+//                    fetching transactions
+                    databaseRef.child("transactions").addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            for (DataSnapshot childSnapshot : snapshot.getChildren()) {
+                                for (DataSnapshot nestedSnapshot : childSnapshot.getChildren()) {
+                                    // Retrieve the key and value of each nested child
+                                    String nestedKey = nestedSnapshot.getKey();
+                                    Object nestedValue = nestedSnapshot.getValue();
+
+                                    // Process the nested child data as needed
+                                    switch(Objects.requireNonNull(nestedKey)) {
+                                        case "username" :
+                                            fetched_name = String.valueOf(Objects.requireNonNull(nestedValue));
+                                            break;
+                                        case "place_location" :
+                                            fetched_location = String.valueOf(Objects.requireNonNull(nestedValue));
+                                            break;
+                                        case "place_name" :
+                                            fetched_location_name = String.valueOf(Objects.requireNonNull(nestedValue));
+                                            break;
+                                        case "total_price" :
+                                            fetched_price = (long) Objects.requireNonNull(nestedValue);
+                                            break;
+                                        case "dateTime" :
+                                            fetched_dateTime = String.valueOf(Objects.requireNonNull(nestedValue));
+                                            break;
+                                        case "email" :
+                                            fetched_email = String.valueOf(Objects.requireNonNull(nestedValue));
+                                            break;
+                                        case "qty" :
+                                            fetched_qty = (int)(long) Objects.requireNonNull(nestedValue);
+                                            break;
+                                    }
+                                }
+                                if (fullName.equals(fetched_name)) transactionList.add(new Transaction(fetched_name, fetched_location_name, fetched_location, fetched_email, fetched_qty, fetched_price));
+                            }
+                            TransactionAdapter adapter = new TransactionAdapter(transactionList, History.this);
+                            rv_history.setLayoutManager(new LinearLayoutManager(History.this));
+                            rv_history.setAdapter(adapter);
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                    clear.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            clearHistory();
+                        }
+                    });
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private  void clearHistory() {
+
     }
 }
--- app/src/main/java/com/example/levelapp/Transaction/TransactionAdapter.java
+++ app/src/main/java/com/example/levelapp/Transaction/TransactionAdapter.java
@@ -1,36 +1,58 @@
 package com.example.levelapp.Transaction;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.levelapp.MainPage.WisataAdapter;
+import com.example.levelapp.R;
+
 import java.util.List;
 
 public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.TransactionViewHolder> {
-    List<Transaction>
+    List<Transaction> transactions;
+    Context context;
+
+    public TransactionAdapter(List<Transaction> transactions, Context context) {
+        this.transactions = transactions;
+        this.context = context;
+    }
 
     @NonNull
     @Override
     public TransactionViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.history, parent, false);
+        return new TransactionViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull TransactionViewHolder holder, int position) {
-
+        Transaction currentTransaction = transactions.get(position);
+        holder.namaWisata.setText(currentTransaction.getPlace_name());
+        holder.lokasiWisata.setText(currentTransaction.getPlace_location());
+        holder.price.setText("Rp" + currentTransaction.getTotal_price());
+        holder.dateTime.setText(currentTransaction.getDateTime());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return transactions.size();
     }
 
     class TransactionViewHolder extends RecyclerView.ViewHolder {
+        TextView namaWisata, lokasiWisata, dateTime, price;
 
         public TransactionViewHolder(@NonNull View itemView) {
             super(itemView);
+            namaWisata = itemView.findViewById(R.id.namaWisata);
+            lokasiWisata = itemView.findViewById(R.id.lokasiWisata);
+            dateTime = itemView.findViewById(R.id.dateTime);
+            price = itemView.findViewById(R.id.price_info);
         }
     }
 }
